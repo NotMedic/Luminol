2a7b62ff5897d9ab84c21aaeffc1cc2b39e7ccc0
Tim McGuffin <17074002+NotMedic@users.noreply.github.com>
2023-04-20 15:30:01 -0500

    Update luminol.ps1

--- luminol.ps1
+++ luminol.ps1
@@ -1,4 +1,17 @@
 Function Invoke-Luminol {
+    #Test if we're running as admin
+    $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    if (-not $isAdmin) {
+        Write-Host "This script must be run as an administrator. Exiting."
+        Exit 1
+    }
+
+    # Test if computer is domain-joined
+    $isDomainJoined = (Get-WmiObject Win32_ComputerSystem).PartOfDomain
+    if (-not $isDomainJoined) {
+        Write-Host "This script must be run on a domain-joined computer. Exiting."
+        Exit 1
+    }
     #Create our base JSON object
     $jsonobject = @{
         "data" = New-Object System.Collections.ArrayList
@@ -11,6 +24,7 @@ Function Invoke-Luminol {
     }
 
     #Create Custom PS Object that matches Bloodhound JSON
+    #Some items are commented out so we don't overwrite them in Neo4j
     $newobject = [PSCustomObject]@{
         Properties         = @{
             domain            = ""
@@ -114,64 +128,64 @@ Function Invoke-Luminol {
 
     #Populate LocalAdmins
     (Get-LocalGroupMember -Group "Administrators").foreach{
-            $adminobject = @{
-                ObjectIdentifier = $_.SID
-                ObjectType       = $_.ObjectClass
-            }
-            $newobject.LocalAdmins.Collected = $true
-            $newobject.LocalAdmins.Results.Add($adminobject) | Out-Null
+        $adminobject = @{
+            ObjectIdentifier = $_.SID
+            ObjectType       = $_.ObjectClass
         }
+        $newobject.LocalAdmins.Collected = $true
+        $newobject.LocalAdmins.Results.Add($adminobject) | Out-Null
+    }
 
     #Populate RemoteDesktopUsers
     (Get-LocalGroupMember -Group "Remote Desktop Users").foreach{
-            $rdpobject = @{
-                ObjectIdentifier = $_.SID
-                ObjectType       = $_.ObjectClass
-            }
-            $newobject.RemoteDesktopUsers.Collected = $true
-            $newobject.RemoteDesktopUsers.Results.Add($rdpobject) | Out-Null
+        $rdpobject = @{
+            ObjectIdentifier = $_.SID
+            ObjectType       = $_.ObjectClass
         }
+        $newobject.RemoteDesktopUsers.Collected = $true
+        $newobject.RemoteDesktopUsers.Results.Add($rdpobject) | Out-Null
+    }
 
     #Populate DcomUsers
     (Get-LocalGroupMember -Group "Distributed COM Users").foreach{
-            $dcomobject = @{
-                ObjectIdentifier = $_.SID
-                ObjectType       = $_.ObjectClass
-            }
-            $newobject.DcomUsers.Collected = $true
-            $newobject.DcomUsers.Results.Add($dcomobject) | Out-Null
+        $dcomobject = @{
+            ObjectIdentifier = $_.SID
+            ObjectType       = $_.ObjectClass
         }
+        $newobject.DcomUsers.Collected = $true
+        $newobject.DcomUsers.Results.Add($dcomobject) | Out-Null
+    }
 
     #Populate PSRemoteUsers
     (Get-LocalGroupMember -Group "Remote Management Users").foreach{
-            $psremoteobject = @{
-                ObjectIdentifier = $_.SID
-                ObjectType       = $_.ObjectClass
-            }
-            $newobject.PSRemoteUsers.Collected = $true
-            $newobject.PSRemoteUsers.Results.Add($psremoteobject) | Out-Null
+        $psremoteobject = @{
+            ObjectIdentifier = $_.SID
+            ObjectType       = $_.ObjectClass
         }
+        $newobject.PSRemoteUsers.Collected = $true
+        $newobject.PSRemoteUsers.Results.Add($psremoteobject) | Out-Null
+    }
 
     #Populate Sessions from Processes
     (Get-Process -IncludeUserName | Select-Object -Unique UserName).foreach{
-            try {
-                $ntAccount = New-Object System.Security.Principal.NTAccount($_.UserName)
-                $sid = $ntAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
+        try {
+            $ntAccount = New-Object System.Security.Principal.NTAccount($_.UserName)
+            $sid = $ntAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
 
-                if ($sid.StartsWith("S-1-5-21")) {
-                    $processobject = @{
-                        UserSID     = $sid
-                        ComputerSID = $newobject.ObjectIdentifier
-                    }
-                    $newobject.PrivilegedSessions.Collected = $true
-                    if ($newobject.PrivilegedSessions.Results -notcontains $processobject) {
-                        $newobject.PrivilegedSessions.Results.Add($processobject) | Out-Null
-                    }
+            if ($sid.StartsWith("S-1-5-21")) {
+                $processobject = @{
+                    UserSID     = $sid
+                    ComputerSID = $newobject.ObjectIdentifier
+                }
+                $newobject.PrivilegedSessions.Collected = $true
+                if ($newobject.PrivilegedSessions.Results -notcontains $processobject) {
+                    $newobject.PrivilegedSessions.Results.Add($processobject) | Out-Null
                 }
             }
-            catch {
-            }
         }
+        catch {
+        }
+    }
 
     #Populate Sessions from Scheduled Tasks
     ForEach ($task in Get-ScheduledTask | Select-Object -ExpandProperty Principal | Where-Object -Property "LogonType" -EQ "Password" | Select-Object -Unique UserId) {
